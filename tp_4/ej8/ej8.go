93fd54e9ed295f58d9f91f382172ab2ea09d74b6
Alfonso Zuccali <alfonso.zuccali@gmail.com>
2025-05-20 14:51:00 -0300

    added buscar function

--- tp_4/ej8/ej8.go
+++ tp_4/ej8/ej8.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 //Nodo
 type Nodo struct {
-	valor     int
 	izquierda *Nodo
+	valor     int
 	derecha   *Nodo
 }
 
@@ -58,6 +60,36 @@ func (arbol *ABB) insertar(input int) {
 	}
 }
 
+//funcion para buscar un elemento en el ABB
+func (arbol *ABB) buscar(input int) bool {
+
+	//creamos el nodo auxiliar
+	observador := arbol.raiz
+
+	//si el arbol esta vacio return false
+	if arbol.EsVacio() {
+		return false
+	} else {
+		//se frena el for loop cuando el valor es encontrado o se llega a una hoja
+		for observador.valor != input && (observador.izquierda != nil || observador.derecha != nil) {
+
+			//si el valor del nodo es mayor al input vamos hacia la izquierda
+			if observador.valor > input && observador.izquierda != nil {
+				observador = observador.izquierda
+
+				// si el valor del nodo es menor al input vamos hacia la derecha
+			} else if observador.valor < input && observador.derecha != nil {
+				observador = observador.derecha
+			}
+		}
+	}
+	if observador.valor == input {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 	arbol := ABB{raiz: nil}
 	arbol.insertar(5)
@@ -66,4 +98,11 @@ func main() {
 	arbol.insertar(3)
 	arbol.insertar(12)
 	arbol.insertar(0)
+
+	fmt.Println("EL valor 2 esta en el arbol: ", arbol.buscar(2))
+	fmt.Println("EL valor 0 esta en el arbol: ", arbol.buscar(0))
+	fmt.Println("EL valor 12 esta en el arbol: ", arbol.buscar(12))
+	fmt.Println("EL valor 3 esta en el arbol: ", arbol.buscar(3))
+	fmt.Println("EL valor 5 esta en el arbol: ", arbol.buscar(5))
+	fmt.Println("EL valor 1 esta en el arbol: ", arbol.buscar(1))
 }
